e20e5e0f45b9f451fad7f9bae5fbace0522ba1e0
mariebleickert@aol.de <mariebleickert@aol.de>
2023-04-19 11:51:39 +0200

    erster Versuch Datenbank

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+// Erforderliche Abhängigkeiten importieren
+var mysql = require('mysql');
+
+// Verbindung zur MySQL-Datenbank herstellen
+var con = mysql.createConnection({
+  host: "MarieBleickert",        // Der Hostname des MySQL-Servers
+  user: "Marie",    // Ihr Benutzername für die MySQL-Datenbank
+  password: "123",// Ihr Passwort für die MySQL-Datenbank
+  database: "rezepte"         // Der Name Ihrer erstellten MySQL-Datenbank
+});
+
+// Verbindung zur Datenbank herstellen
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to MySQL database!");
+  
+  // Hier können Sie Ihre MySQL-Datenbank-Operationen durchführen
+});
+
+  // Daten für die INSERT-Abfrage
+  var data = {name: "Röstapfel", Sterne: 1 , Eigenschaft: "Herzen"};
+  
+  // INSERT-Abfrage ausführen
+  con.query("INSERT INTO tablename SET ?", data, function (err, result) {
+    if (err) throw err;
+    console.log("Data inserted:", result);
+  });
+
