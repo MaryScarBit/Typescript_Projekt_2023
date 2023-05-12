419ac7019b9434245b1d2d5de570c12df480ebc1
MaryScarBit <mariebleickert@aol.de>
2023-05-12 17:20:11 +0200

    Comments

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,78 @@
-document.addEventListener("DOMContentLoaded", () : void=>{
-    const boxes : NodeListOf<HTMLDivElement> = document.querySelectorAll(".box")
-    let items : NodeListOf<HTMLDivElement> = document.querySelectorAll(".item")
-    boxes.forEach((b : HTMLDivElement) => {
-        b.addEventListener("drop", handleItemDrop)
-        b.addEventListener("dragover", (ev : DragEvent)=> {
-            ev.preventDefault();
-        })
+//Logic: wird ausgeführt, wenn DOM geladen ist. 1. EventListeners für D&D, 2.update, 3. buttons
+
+
+document.addEventListener("DOMContentLoaded", (): void => {
+  const boxes: NodeListOf<HTMLDivElement> = document.querySelectorAll(".box")
+  let items: NodeListOf<HTMLDivElement> = document.querySelectorAll(".item")
+  boxes.forEach((b: HTMLDivElement) => {
+    b.addEventListener("drop", handleItemDrop)
+    b.addEventListener("dragover", (ev: DragEvent) => {
+      ev.preventDefault();
     })
-    update();
-    const buttons : NodeListOf<HTMLButtonElement> | null = document.querySelectorAll(".btn-add")
-    if (buttons !== null) {
-        buttons.forEach((y : HTMLButtonElement) => {
-            y.addEventListener('click' , addElement)
-        })
-    }
+  })
+  update();
+  const buttons: NodeListOf<HTMLButtonElement> | null = document.querySelectorAll(".btn-add")
+  if (buttons !== null) {
+    buttons.forEach((y: HTMLButtonElement) => {
+      y.addEventListener('click', addElement)
+    })
+  }
 })
 
 
 /*
-Die EventListener für Drag und Drop verschwinden für die Objekte, die ich neu hinzugefügt habe. Demnach muss ich mit einer Funktion
-diese wieder laufend hinzufügen. Sonst wird Drag nicht aufgerufen.
+Stellt sicher, dass D&D für neu hinzugefügte Elemente funktioniert. Sonst wird Drag nicht aufgerufen
  */
 function update() {
-    let items: NodeListOf<HTMLDivElement> = document.querySelectorAll(".item")
-    items.forEach((i: HTMLDivElement) => {
-        i.addEventListener("drag", (ev : DragEvent)=> {ev.preventDefault()})
-        i.addEventListener("dragstart", handleItemDrag)
-    })
+  let items: NodeListOf<HTMLDivElement> = document.querySelectorAll(".item")
+  items.forEach((i: HTMLDivElement) => {
+    i.addEventListener("drag", (ev: DragEvent) => { ev.preventDefault() })
+    i.addEventListener("dragstart", handleItemDrag)
+  })
 }
 /*
-Wird ausgeführt, wenn man ein Item über den Boxen fallenlässt. Ich erstelle ein frisches Element und schmeiße es in die
+Wird ausgeführt, wenn man ein Item über den Boxen fallenlässt: es folgt ein frisches Element in der
 entsprechende Box
  */
 function handleItemDrop(this: HTMLDivElement, ev: DragEvent) {
-    if (ev.dataTransfer !== null) {
-        const itemtitle : string = ev.dataTransfer.getData("text/plain")
-        let elem = document.createElement("div")
-        elem.setAttribute("contentEditable","true")
-        elem.innerHTML = itemtitle
-        elem.classList.add("item")
-        elem.setAttribute("draggable", "true");
-        let old : HTMLDivElement | null = document.querySelector(".dragged");
-        if (old !== null && old.parentNode !== null) {
-            old.parentNode.removeChild(old);
-        }
-        this.appendChild(elem);
-        update();
+  if (ev.dataTransfer !== null) {
+    const itemtitle: string = ev.dataTransfer.getData("text/plain")
+    let elem = document.createElement("div")
+    elem.setAttribute("contentEditable", "true")
+    elem.innerHTML = itemtitle
+    elem.classList.add("item")
+    elem.setAttribute("draggable", "true");
+    let old: HTMLDivElement | null = document.querySelector(".dragged");
+    if (old !== null && old.parentNode !== null) {
+      old.parentNode.removeChild(old);
     }
-}
-/*
-Ich kopiere hier den Inhalt des items in den datatransfer und übertrage den so ins Drop Event
- */
-function handleItemDrag(this: HTMLDivElement, ev: DragEvent) {
-    if (ev.dataTransfer !== null) {
-        ev.dataTransfer.setData("text/plain", this.innerHTML)
-        ev.dataTransfer.dropEffect = "move"
-        this.classList.add("dragged")
-    }
+    this.appendChild(elem);
+    update();
+  }
 }
 
+//Ich kopiere hier den Inhalt des items in den datatransfer und übertrage den so ins Drop Event
+
+function handleItemDrag(this: HTMLDivElement, ev: DragEvent) {
+  if (ev.dataTransfer !== null) {
+    ev.dataTransfer.setData("text/plain", this.innerHTML)
+    ev.dataTransfer.dropEffect = "move"
+    this.classList.add("dragged")
+  }
+}
+
+// erstellt ein neues Element und fügt es dem übergeordneten Container hinzu
 function addElement(this: HTMLButtonElement) {
   let elem = document.createElement("div")
-      elem.setAttribute("contentEditable", "true")
-      elem.classList.add("item")
-      elem.setAttribute("draggable", "true");
-      let container : HTMLDivElement | null = this.parentNode as HTMLDivElement
-      if (container !== null) {
-          container = container.parentNode as HTMLDivElement
-          container.appendChild(elem)
-      }
-      update()
-    }
+  elem.setAttribute("contentEditable", "true")
+  elem.classList.add("item")
+  elem.setAttribute("draggable", "true");
+  let container: HTMLDivElement | null = this.parentNode as HTMLDivElement
+  if (container !== null) {
+    container = container.parentNode as HTMLDivElement
+    container.appendChild(elem)
+  }
+  update()
+}
 
 
